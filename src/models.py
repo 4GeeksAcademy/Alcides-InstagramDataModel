457f5689fa817522c9f8f785dcab6efffb4db4de
Alcides Rivera <elarsi626@gmail.com>
2026-01-30 19:40:34 +0000

    First

--- src/models.py
+++ src/models.py
@@ -1,19 +1,30 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = "user"
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
-
+    first_name: Mapped[str] = mapped_column(nullable=False)
+    last_name: Mapped[str | None] = mapped_column(nullable=True)
+    posts: Mapped[list["Post"]] = relationship(back_populates="user")
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
             # do not serialize the password, its a security breach
         }
+
+class Post(db.Model):
+    __tablename__ = "posts"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"), nullable=False)
+    caption: Mapped[str] = mapped_column(nullable=False)
+    image: Mapped[str] = mapped_column(nullable=False)
+    user: Mapped["User"] = relationship(back_populates="posts")
